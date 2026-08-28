8d348c72efb1577b5335db8f754e7ee91ad77041
amitkhan3 <amitmkhan3@gmail.com>
2026-08-28 12:13:05 +0600

    Add V3 layout

---
+++ app/layout.jsx
@@ -0,0 +1,3 @@
+import './globals.css';
+export const metadata={title:'AdPage Builder V3',description:'Create and publish landing pages'};
+export default function RootLayout({children}){return <html lang='en'><body>{children}</body></html>}
