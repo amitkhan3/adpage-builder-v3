da8ebb667cced90f1c63f21da883c4940a7dc802
amitkhan3 <amitmkhan3@gmail.com>
2026-09-01 00:56:27 +0600

    Add Clerk sign in page

---
+++ app/sign-in/[[...sign-in]]/page.jsx
@@ -0,0 +1,2 @@
+import { SignIn } from '@clerk/nextjs';
+export default function Page(){return <main style={{minHeight:'100vh',display:'grid',placeItems:'center',padding:24}}><SignIn routing='path' path='/sign-in' signUpUrl='/sign-up'/></main>}
