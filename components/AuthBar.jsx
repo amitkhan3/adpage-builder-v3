a3672619ca567d25d2a0539bc09bd6b877b9c913
amitkhan3 <amitmkhan3@gmail.com>
2026-09-01 02:10:36 +0600

    Make subscription plans link clearly visible

--- components/AuthBar.jsx
+++ components/AuthBar.jsx
@@ -3,13 +3,12 @@ import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
 
 export default function AuthBar(){
   return <div className="auth-bar">
+    <a className="auth-subscription" href="/subscription">💳 Subscription Plans</a>
     <SignedOut>
-      <a className="auth-subscription" href="/subscription">💳 Subscription</a>
       <SignInButton mode="modal"><button className="auth-signin">Login</button></SignInButton>
       <a className="auth-signup" href="/sign-up">Create account</a>
     </SignedOut>
     <SignedIn>
-      <a className="auth-subscription" href="/subscription">💳 Subscription</a>
       <a className="auth-dashboard" href="/">Dashboard</a>
       <UserButton afterSignOutUrl="/sign-in" />
     </SignedIn>
