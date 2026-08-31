b5eb4543a1ba4f97f8fac2aef912ab33e2709c23
amitkhan3 <amitmkhan3@gmail.com>
2026-09-01 00:56:13 +0600

    Protect dashboard and API routes with Clerk

---
+++ middleware.js
@@ -0,0 +1,6 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+const isPublic = createRouteMatcher(['/p(.*)', '/sign-in(.*)', '/sign-up(.*)']);
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublic(req)) await auth.protect();
+});
+export const config={matcher:['/((?!_next|.*\\..*).*)','/api/(.*)']};
