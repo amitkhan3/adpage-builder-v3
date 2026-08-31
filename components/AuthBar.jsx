06385fc8caba3efb6d9f92aa29579af6d80fce77
amitkhan3 <amitmkhan3@gmail.com>
2026-09-01 02:03:35 +0600

    Add visible subscription payment link

--- components/AuthBar.jsx
+++ components/AuthBar.jsx
@@ -3,7 +3,15 @@ import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
 
 export default function AuthBar(){
   return <div className="auth-bar">
-    <SignedOut><SignInButton mode="modal"><button className="auth-signin">Login</button></SignInButton><a className="auth-signup" href="/sign-up">Create account</a></SignedOut>
-    <SignedIn><a className="auth-dashboard" href="/">Dashboard</a><UserButton afterSignOutUrl="/sign-in" /></SignedIn>
+    <SignedOut>
+      <a className="auth-subscription" href="/subscription">💳 Subscription</a>
+      <SignInButton mode="modal"><button className="auth-signin">Login</button></SignInButton>
+      <a className="auth-signup" href="/sign-up">Create account</a>
+    </SignedOut>
+    <SignedIn>
+      <a className="auth-subscription" href="/subscription">💳 Subscription</a>
+      <a className="auth-dashboard" href="/">Dashboard</a>
+      <UserButton afterSignOutUrl="/sign-in" />
+    </SignedIn>
   </div>;
 }
